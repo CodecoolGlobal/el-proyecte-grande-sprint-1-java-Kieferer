467803fe234b420b59eedec52b0427ce7032e223
Norbert Remenyi <remenyinorbert@gmail.com>
2023-04-25 12:07:12 +0200

    feat: Add Client entity and ClientRepository

---
+++ src/main/java/com/codecool/budapestgo/data/client/Client.java
@@ -0,0 +1,28 @@
+package com.codecool.budapestgo.data.client;
+
+import com.codecool.budapestgo.data.ClientCategoryType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@Entity
+public class Client {
+    @Id
+    @SequenceGenerator(
+            name = "client_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE
+    )
+    private final Integer id;
+    @Enumerated(EnumType.STRING)
+    @NonNull private final ClientCategoryType type;
+    @NonNull private String email;
+    @NonNull private String password;
+    public void setPassword(@NonNull String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/codecool/budapestgo/data/client/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.codecool.budapestgo.data.client;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client,Integer> {
+
+}
