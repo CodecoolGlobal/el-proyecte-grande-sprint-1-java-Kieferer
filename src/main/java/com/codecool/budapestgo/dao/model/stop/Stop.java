7de26e3a9bbc4086da8fca3e935f60db6cab2234
Norbert Remenyi <remenyinorbert@gmail.com>
2023-04-25 19:13:35 +0200

    feat: add Stop entity

---
+++ src/main/java/com/codecool/budapestgo/dao/model/stop/Stop.java
@@ -0,0 +1,24 @@
+package com.codecool.budapestgo.dao.model.stop;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor(force = true)
+@AllArgsConstructor()
+@Entity
+public class Stop {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE
+    )
+    private final Integer id;
+    @NonNull private String name;
+    @Embedded
+    @NonNull private Point location;
+
+
+}
