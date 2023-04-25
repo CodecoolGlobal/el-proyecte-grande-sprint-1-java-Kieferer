8e1c971372a289290eee66f58d88b564a30e910f
klavoraMate <klavoramate98@gmail.com>
2023-04-25 21:08:21 +0200

    feat: created service class for routes and Route class implemented

--- src/main/java/com/codecool/budapestgo/service/RouteService.java
+++ src/main/java/com/codecool/budapestgo/service/RouteService.java
@@ -3,24 +3,47 @@ package com.codecool.budapestgo.service;
 import com.codecool.budapestgo.controller.dto.NewRouteDTO;
 import com.codecool.budapestgo.controller.dto.RouteDTO;
 import com.codecool.budapestgo.controller.dto.UpdateRouteDTO;
+import com.codecool.budapestgo.dao.Route.Route;
+import com.codecool.budapestgo.dao.Route.RouteRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class RouteService {
-    public List<RouteDTO> getAllRoutes() {
-        return null;
+    private final RouteRepository routeRepository;
+
+    public RouteService(RouteRepository routeRepository) {
+        this.routeRepository = routeRepository;
     }
+
+    public List<RouteDTO> getAllRoutes() {
+        return routeRepository.findAll().stream().map(RouteDTO::new).toList();
+    }
+
 
     public void deleteRoute(int id) {
-
+        routeRepository.deleteById(id);
     }
 
-    public void addRoute(NewRouteDTO newRoute) {
-
+    public void addRoute(NewRouteDTO newRouteDTO) {
+        Route route = Route.builder()
+                .name(newRouteDTO.name())
+                .build();
+        routeRepository.save(route);
     }
 
     public ResponseEntity<String> updateRoute(UpdateRouteDTO newRouteDTO) {
-        return null;
+        Optional<Route> route = routeRepository.findById(newRouteDTO.id());
+        if (route.isPresent()){
+            route.get().setName(newRouteDTO.name());
+            routeRepository.save(route.get());
+            return ResponseEntity.ok("Route updated");
+        }
+        else
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Route not found");
     }
 }
