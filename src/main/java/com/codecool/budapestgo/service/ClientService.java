eb802c51172cb47ec95bdd657ec77ea28ae8bfaa
Norbert Remenyi <remenyinorbert@gmail.com>
2023-04-25 15:44:47 +0200

    feat: add update to ClientService

--- src/main/java/com/codecool/budapestgo/service/ClientService.java
+++ src/main/java/com/codecool/budapestgo/service/ClientService.java
@@ -1,8 +1,13 @@
 package com.codecool.budapestgo.service;
 
 import com.codecool.budapestgo.controller.dto.ClientDTO;
+import com.codecool.budapestgo.controller.dto.ClientRegisterDTO;
+import com.codecool.budapestgo.controller.dto.ClientUpdateDTO;
 import com.codecool.budapestgo.dao.client.Client;
 import com.codecool.budapestgo.dao.client.ClientRepository;
+import com.codecool.budapestgo.data.ClientCategoryType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,16 +21,33 @@ public class ClientService {
         this.clientRepository = clientRepository;
     }
     public List<ClientDTO> getAllClient(){
-    return clientRepository.findAll().stream().map(ClientDTO::of).toList();
+    return clientRepository.findAll()
+                           .stream()
+                           .map(ClientDTO::of)
+                           .toList();
     }
     public Optional<ClientDTO> getClientById(Integer id){
-    return clientRepository.findById(id).map(ClientDTO::of);
+        return clientRepository.findById(id)
+                .map(ClientDTO::of);
     }
     public void deleteClientById(Integer id){
         clientRepository.deleteById(id);
     }
-    public void addClient(Client client){
+    public void addClient(ClientRegisterDTO clientToRegister){
+        Client client = Client.builder()
+                .email(clientToRegister.email())
+                .password(clientToRegister.password())
+                .type(ClientCategoryType.CUSTOMER)
+                .build();
         clientRepository.save(client);
     }
-
+    public ResponseEntity<String> updateClient(ClientUpdateDTO updateClient){
+        Optional<Client> client = clientRepository.findById(updateClient.id());
+        if(client.isPresent()) {
+            client.get().setPassword(updateClient.password());
+            clientRepository.save(client.get());
+            return ResponseEntity.ok("Client updated");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Client not found");
+    }
 }
