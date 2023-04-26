8baf706de88ecb2b014ed85bef55200af761aced
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-04-26 11:53:01 +0200

    feat: Add id, route and stop fields to Schedule Entity

--- src/main/java/com/codecool/budapestgo/dao/model/schedule/Schedule.java
+++ src/main/java/com/codecool/budapestgo/dao/model/schedule/Schedule.java
@@ -1,4 +1,20 @@
 package com.codecool.budapestgo.dao.model.schedule;
 
+import com.codecool.budapestgo.dao.model.route.Route;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+@NoArgsConstructor(force = true)
+@Entity
 public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private final int id;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "routeId", referencedColumnName = "id", nullable = false)
+    private Route route;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "stopId", referencedColumnName = "id", nullable = false)
+    private Route stop;
 }
