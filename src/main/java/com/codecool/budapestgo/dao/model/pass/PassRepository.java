8f3f9d143c2ed246e771a76957f33cc9eb7115dc
Norbert Remenyi <remenyinorbert@gmail.com>
2023-04-26 19:53:54 +0200

    chore : merge route,client,stop,schedule branch

--- src/main/java/com/codecool/budapestgo/dao/model/pass/PassRepository.java
+++ src/main/java/com/codecool/budapestgo/dao/model/pass/PassRepository.java
@@ -7,8 +7,8 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface PassRepository extends JpaRepository<Pass,Integer> {
-    @Query("SELECT p FROM Pass p WHERE p.startTime < :date AND  p.client_id = :id")
+    @Query("SELECT p FROM Pass p WHERE p.startTime < :date AND  p.client.id = :id")
     List<Pass> getAllExpiredPassesByClient_id(Integer id,LocalDate date);
-    @Query("SELECT p FROM Pass p WHERE p.startTime > :date AND  p.client_id = :id")
+    @Query("SELECT p FROM Pass p WHERE p.startTime > :date AND  p.client.id = :id")
     List<Pass> getActivePassesByClient_id(Integer id,LocalDate date);
 }
--- src/main/java/com/codecool/budapestgo/service/PassService.java
+++ src/main/java/com/codecool/budapestgo/service/PassService.java
@@ -2,38 +2,47 @@ package com.codecool.budapestgo.service;
 
 import com.codecool.budapestgo.controller.dto.pass.PassDTO;
 import com.codecool.budapestgo.controller.dto.pass.PassResponseDTO;
+import com.codecool.budapestgo.dao.model.client.Client;
+import com.codecool.budapestgo.dao.model.client.ClientRepository;
 import com.codecool.budapestgo.dao.model.pass.Pass;
 import com.codecool.budapestgo.dao.model.pass.PassRepository;
 import com.codecool.budapestgo.dao.model.pass.PassType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PassService {
     private final PassRepository passRepository;
-
-    public PassService(PassRepository passRepository) {
+    private final ClientRepository clientRepository;
+    public PassService(PassRepository passRepository, ClientRepository clientRepository) {
         this.passRepository = passRepository;
+        this.clientRepository = clientRepository;
     }
     public List<PassResponseDTO> getAllPass(){
         return passRepository.findAll().stream().map(PassResponseDTO::of) .toList();
     }
 
     public ResponseEntity<String> addPass(PassDTO passDTO){
-        PassType type = PassType.valueOf(passDTO.passType());
+        Optional<Client> client = clientRepository.findById(passDTO.clientId());
+        if(client.isPresent()) {
+            PassType type = PassType.valueOf(passDTO.passType());
+            Pass pass = Pass.builder()
+                    .client(client.get())
+                    .passType(type)
+                    .startTime(LocalDate.now())
+                    .expireTime(Pass.calculateExpireTime(LocalDate.now(), type))
+                    .build();
 
-        Pass pass = Pass.builder()
-                        .client_id(passDTO.clientId())
-                        .passType(type)
-                        .startTime(LocalDate.now())
-                        .expireTime(Pass.calculateExpireTime(LocalDate.now(),type))
-                        .build();
-
-        passRepository.save(pass);
-        return ResponseEntity.ok("Pass have been purchased");
+            passRepository.save(pass);
+            return ResponseEntity.ok("Pass have been purchased");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Client not found.");
     }
     public List<PassResponseDTO> getExpiredPasses(Integer id){
         return passRepository.getAllExpiredPassesByClient_id(id,LocalDate.now())
