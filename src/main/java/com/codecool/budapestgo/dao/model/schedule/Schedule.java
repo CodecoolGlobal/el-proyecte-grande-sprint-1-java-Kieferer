5f4ef37f3755baa9f2491cb5335b7fc106f208db
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-04-26 14:38:14 +0200

    feat: Added missing annotations and changed paramaters in JoinColumn.

--- src/main/java/com/codecool/budapestgo/dao/model/schedule/Schedule.java
+++ src/main/java/com/codecool/budapestgo/dao/model/schedule/Schedule.java
@@ -1,9 +1,16 @@
 package com.codecool.budapestgo.dao.model.schedule;
 
 import com.codecool.budapestgo.dao.model.route.Route;
+import com.codecool.budapestgo.dao.model.stop.Stop;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(force = true)
+@Getter
 @Entity
 public class Schedule {
     @Id
@@ -11,10 +18,12 @@ public class Schedule {
     private final int id;
 
     @ManyToOne(cascade = CascadeType.REMOVE)
-    @JoinColumn(name = "routeId", referencedColumnName = "id", nullable = false)
+    @JoinColumn(name = "route_name", referencedColumnName = "name")
     private Route route;
 
     @ManyToOne(cascade = CascadeType.REMOVE)
-    @JoinColumn(name = "stopId", referencedColumnName = "id", nullable = false)
-    private Route stop;
+    @JoinColumn(name = "stop_name", referencedColumnName = "name")
+    private Stop stop;
+
+    //duration, launch_date
 }
