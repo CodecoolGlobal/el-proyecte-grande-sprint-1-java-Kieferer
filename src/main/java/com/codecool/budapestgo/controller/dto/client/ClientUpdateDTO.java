fa22a332fc396ab96c3da447fe3f43df4f8efdfa
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-05-09 11:38:33 +0200

    feat: Add static 'of' method and id changed to Long from Integer

--- src/main/java/com/codecool/budapestgo/controller/dto/client/ClientUpdateDTO.java
+++ src/main/java/com/codecool/budapestgo/controller/dto/client/ClientUpdateDTO.java
@@ -1,4 +1,9 @@
 package com.codecool.budapestgo.controller.dto.client;
 
-public record ClientUpdateDTO (Integer id, String password){
+import com.codecool.budapestgo.dao.model.Client;
+
+public record ClientUpdateDTO (Long id, String password){
+    public static ClientUpdateDTO of(Client client){
+        return new ClientUpdateDTO(client.getId(), client.getPassword());
+    }
 }
