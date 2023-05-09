eaaa6c4b798c80472dae0b991c5f975aae8520e9
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-05-09 12:16:25 +0200

    chore: Add @Repository annotation to repositories

--- src/main/java/com/codecool/budapestgo/dao/repository/ScheduleRepository.java
+++ src/main/java/com/codecool/budapestgo/dao/repository/ScheduleRepository.java
@@ -4,9 +4,10 @@ import com.codecool.budapestgo.dao.model.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     @Query("SELECT s FROM Schedule s WHERE s.route.name = :name")
     List<Schedule> findByRouteName(@Param("name") String name);
--- src/main/java/com/codecool/budapestgo/dao/repository/StopRepository.java
+++ src/main/java/com/codecool/budapestgo/dao/repository/StopRepository.java
@@ -3,9 +3,10 @@ package com.codecool.budapestgo.dao.repository;
 import com.codecool.budapestgo.dao.model.Stop;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface StopRepository extends JpaRepository<Stop, Long> {
     Optional<Stop> getStopByName(@NonNull String name);
 }
