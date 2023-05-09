131b128984ae87d794ae531d32ad9e9b6b325a62
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-05-09 11:47:03 +0200

    chore: Class replaced by record and add static 'of' method

--- src/main/java/com/codecool/budapestgo/controller/dto/route/RouteDTO.java
+++ src/main/java/com/codecool/budapestgo/controller/dto/route/RouteDTO.java
@@ -4,42 +4,8 @@ import com.codecool.budapestgo.dao.model.Route;
 
 import java.util.Objects;
 
-public final class RouteDTO {
-    private final int id;
-    private final String name;
-
-    public RouteDTO(Route route) {
-        this.id = route.getId();
-        this.name = route.getName();
+public record RouteDTO(Long id, String name) {
+    public static RouteDTO of(Route route){
+        return new RouteDTO(route.getId(), route.getName());
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (RouteDTO) obj;
-        return this.id == that.id &&
-                Objects.equals(this.name, that.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
-
-    @Override
-    public String toString() {
-        return "RouteDTO[" +
-                "id=" + id + ", " +
-                "name=" + name + ']';
-    }
-
 }
