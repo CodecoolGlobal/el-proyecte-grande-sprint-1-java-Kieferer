ea9a9a19fcb9d56a593b29dd7181be2804634c00
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-05-09 11:40:21 +0200

    chore: Class replaced by record and add static 'of' method

--- src/main/java/com/codecool/budapestgo/controller/dto/pass/PassResponseDTO.java
+++ src/main/java/com/codecool/budapestgo/controller/dto/pass/PassResponseDTO.java
@@ -1,46 +1,11 @@
 package com.codecool.budapestgo.controller.dto.pass;
 
 import com.codecool.budapestgo.dao.model.Pass;
-import lombok.Getter;
+import com.codecool.budapestgo.dao.types.PassType;
 
 import java.time.LocalDate;
-import java.util.Objects;
-@Getter
-public final class PassResponseDTO {
-    private final String passType;
-    private final LocalDate startDate;
-    private final LocalDate expirationDate;
-
-    public PassResponseDTO(String passType, LocalDate startDate, LocalDate expirationDate) {
-        this.passType = passType;
-        this.startDate = startDate;
-        this.expirationDate = expirationDate;
-    }
-
+public record PassResponseDTO(String passType, LocalDate startDate, LocalDate expirationDate) {
     public static PassResponseDTO of(Pass pass){
-        return new PassResponseDTO(pass.getPassType().toString(),pass.getStartTime(),pass.getExpireTime());
+        return new PassResponseDTO(pass.getPassType().name(), pass.getStartTime(), pass.getExpireTime());
     }
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (PassResponseDTO) obj;
-        return Objects.equals(this.passType, that.passType) &&
-                Objects.equals(this.startDate, that.startDate) &&
-                Objects.equals(this.expirationDate, that.expirationDate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(passType, startDate, expirationDate);
-    }
-
-    @Override
-    public String toString() {
-        return "PassResponseDTO[" +
-                "passType=" + passType + ", " +
-                "startDate=" + startDate + ", " +
-                "expirationDate=" + expirationDate + ']';
-    }
-
 }
