70a82746fadee1b92b6283beb3e509d13616b482
Norbert Remenyi <remenyinorbert@gmail.com>
2023-05-10 11:31:19 +0200

    Chore: Add custom validation to PurchasedPassController

--- src/main/java/com/codecool/budapestgo/controller/PurchasedPassController.java
+++ src/main/java/com/codecool/budapestgo/controller/PurchasedPassController.java
@@ -2,13 +2,13 @@ package com.codecool.budapestgo.controller;
 
 import com.codecool.budapestgo.controller.dto.pass.PassDTO;
 import com.codecool.budapestgo.controller.dto.pass.PurchasedPassResponseDTO;
-import com.codecool.budapestgo.controller.dto.validator.DTOValidator;
 import com.codecool.budapestgo.service.PurchasedPassService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 @RestController
@@ -23,29 +23,21 @@ public class PurchasedPassController {
     }
 
     @GetMapping("/active/{id}")
-    public List<PurchasedPassResponseDTO> getActivePasses(@PathVariable Long id){
+    public List<PurchasedPassResponseDTO> getActivePasses(@Valid @PathVariable @Min(1) Long id){
         return purchasedPassService.getActivePasses(id);
     }
     @GetMapping("/expired/{id}")
-    public List<PurchasedPassResponseDTO> getExpiredPasses(@PathVariable Long id){
+    public List<PurchasedPassResponseDTO> getExpiredPasses(@Valid @PathVariable @Min(1) Long id){
         return purchasedPassService.getExpiredPasses(id);
     }
 
     @PostMapping("/register")
-    public ResponseEntity<String> registerPass(@RequestBody PassDTO passDTO){
-        try {
-            if (DTOValidator.registrationIsInvalid(passDTO)) {
-                return ResponseEntity.badRequest().body("Fields cannot be empty");
-            } else {
+    public ResponseEntity<String> registerPass(@Valid @RequestBody PassDTO passDTO){
                 return purchasedPassService.addPass(passDTO);
-            }
-        }catch (IllegalAccessException | InvocationTargetException e) {
-                throw new RuntimeException(e);
-        }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteAllPassOfClient(@PathVariable Long id){
+    public ResponseEntity<String> deleteAllPassOfClient(@Valid @PathVariable @Min(1) Long id){
        return purchasedPassService.deletePassesByClientId(id);
     }
 }
