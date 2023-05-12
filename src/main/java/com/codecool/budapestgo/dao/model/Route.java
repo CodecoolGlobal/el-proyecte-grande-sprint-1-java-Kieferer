3b43fdadf5eca24b8fc13cea0b709047527fda49
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-05-12 03:25:55 +0200

    refactor: Explicit initialization of schedules list

--- src/main/java/com/codecool/budapestgo/dao/model/Route.java
+++ src/main/java/com/codecool/budapestgo/dao/model/Route.java
@@ -3,6 +3,7 @@ package com.codecool.budapestgo.dao.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -24,7 +25,7 @@ public class Route {
     private final String name;
     @JsonIgnore
     @OneToMany(mappedBy = "route", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Schedule> schedules;
+    private List<Schedule> schedules = new ArrayList<>();
     public void addSchedule(Schedule schedule) {
         schedules.add(schedule);
         schedule.setRoute(this);
