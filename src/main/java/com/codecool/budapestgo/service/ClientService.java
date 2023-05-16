f907efee7a44556f21fddb36ffda22a51746497c
Norbert Remenyi <remenyinorbert@gmail.com>
2023-05-16 12:43:42 +0200

    Chore: ClientService use DtoMapper, code repetition replaced by method

--- src/main/java/com/codecool/budapestgo/service/ClientService.java
+++ src/main/java/com/codecool/budapestgo/service/ClientService.java
@@ -3,23 +3,23 @@ package com.codecool.budapestgo.service;
 import com.codecool.budapestgo.controller.dto.client.ClientDTO;
 import com.codecool.budapestgo.controller.dto.client.ClientRegisterDTO;
 import com.codecool.budapestgo.controller.dto.client.ClientUpdateDTO;
+import com.codecool.budapestgo.customExceptionHandler.NotFoundException;
 import com.codecool.budapestgo.dao.model.Client;
 import com.codecool.budapestgo.dao.repository.ClientRepository;
-import com.codecool.budapestgo.dao.types.ClientCategoryType;
-import org.springframework.http.HttpStatus;
+import com.codecool.budapestgo.dao.repository.PurchasedPassRepository;
+import com.codecool.budapestgo.utils.DtoMapper;
+import com.codecool.budapestgo.utils.Response;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class ClientService {
     private final ClientRepository clientRepository;
-
-    public ClientService(ClientRepository clientRepository) {
-        this.clientRepository = clientRepository;
-    }
+    private final PurchasedPassRepository passRepository;
     public List<ClientDTO> getAllClient(){
         return clientRepository.findAll()
                 .stream()
@@ -27,53 +27,32 @@ public class ClientService {
                 .toList();
     }
     public ResponseEntity<String> deleteClientById(Long id){
-        if(isClientExistById(id)) {
-            clientRepository.deleteById(id);
-            return ResponseEntity.ok("Client deleted");
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Invalid id");
+        Client client = clientRepository.findById(id).orElseThrow(() -> new NotFoundException("Client with id " + id));
+        passRepository.deleteAllByClient_Id(client.getId());
+        clientRepository.deleteById(id);
+        return Response.successful("Deleted");
     }
     public ResponseEntity<String> addClient(ClientRegisterDTO clientRegisterDTO){
-                if (getClientByEmail(clientRegisterDTO.email()).isEmpty()) {
-                    Client client = customerOf(clientRegisterDTO);
-                    clientRepository.save(client);
-                    return ResponseEntity.ok("User created");
-                }
-                return ResponseEntity.badRequest().body("User with that email already exist.");
+        Client client = DtoMapper.toEntity(clientRegisterDTO);
+        clientRepository.save(client);
+        return Response.created("Client");
     }
     public ResponseEntity<String> updateClient(ClientUpdateDTO updateClient){
-        Optional<Client> client = clientRepository.findById(updateClient.id());
-        if(client.isPresent()) {
-            client.get().setPassword(updateClient.password());
-            clientRepository.save(client.get());
-            return ResponseEntity.ok("Client updated");
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Client not found");
+        Client client = clientRepository.findById(updateClient.id()).orElseThrow(() -> new NotFoundException("Client with id " + updateClient.id()));
+        client.setPassword(updateClient.password());
+        clientRepository.save(client);
+        return Response.successful("Updated");
     }
 
     public ResponseEntity<Client> login(String email, String password) {
-        Optional<Client> client = getClientByEmail(email);
-        if (client.isPresent()){
-            if (client.get().getPassword().equals(password))
-                return ResponseEntity.ok(client.get());
+        Client client = getClientByEmail(email);
+            if (client.getPassword().equals(password))
+                return ResponseEntity.ok(client);
             else
                 return ResponseEntity.badRequest().build();
-        }
-        else
-            return ResponseEntity.notFound().build();
     }
 
-    private Client customerOf(ClientRegisterDTO clientRegisterDTO){
-        return Client.builder()
-                .email(clientRegisterDTO.email())
-                .password(clientRegisterDTO.password())
-                .type(ClientCategoryType.CUSTOMER)
-                .build();
-    }
-    private Optional<Client> getClientByEmail(String email){
-        return clientRepository.findClientByEmail(email);
-    }
-    private boolean isClientExistById(Long id){
-        return clientRepository.findById(id).isPresent();
+    private Client getClientByEmail(String email){
+        return clientRepository.findClientByEmail(email).orElseThrow(() -> new RuntimeException("Wrong username or password"));
     }
 }
