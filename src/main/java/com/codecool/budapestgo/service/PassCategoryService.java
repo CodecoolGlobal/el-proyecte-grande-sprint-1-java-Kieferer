1a82bd3e25b40457cd9160c979556152c31d2f9c
Norbert Remenyi <remenyinorbert@gmail.com>
2023-05-16 12:44:10 +0200

    Chore: PassCategoryService use DtoMapper, code repetition replaced by method

--- src/main/java/com/codecool/budapestgo/service/PassCategoryService.java
+++ src/main/java/com/codecool/budapestgo/service/PassCategoryService.java
@@ -2,17 +2,20 @@ package com.codecool.budapestgo.service;
 
 import com.codecool.budapestgo.controller.dto.pass.PassCategoryRegisterDTO;
 import com.codecool.budapestgo.controller.dto.pass.PassCategoryResponseDTO;
+import com.codecool.budapestgo.customExceptionHandler.NotFoundException;
 import com.codecool.budapestgo.dao.model.PassCategory;
 import com.codecool.budapestgo.dao.repository.PassCategoryRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
+import com.codecool.budapestgo.utils.DtoMapper;
+import com.codecool.budapestgo.utils.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class PassCategoryService {
     private final PassCategoryRepository passCategoryRepository;
 
@@ -23,49 +26,31 @@ public class PassCategoryService {
                 .toList();
     }
     public ResponseEntity<String> addPassCategory(PassCategoryRegisterDTO passCategoryRegisterDTO){
-          if (isCategoryNotExist(passCategoryRegisterDTO)) {
-              PassCategory passCategory = passCategoryOf(passCategoryRegisterDTO);
-              passCategoryRepository.save(passCategory);
-              return ResponseEntity.ok("Pass category saved.");
-          }
-          return ResponseEntity.badRequest().body("Pass category already exist.");
+        validateCategoryNotExist(passCategoryRegisterDTO);
+        PassCategory passCategory = DtoMapper.toEntity(passCategoryRegisterDTO);
+        passCategoryRepository.save(passCategory);
+
+        return Response.created("Pass category");
     }
     public ResponseEntity<String> deletePassCategoryById(Long id){
-        if(isCategoryExistById(id)) {
-            passCategoryRepository.deleteById(id);
-            return ResponseEntity.ok("Pass category deleted.");
-        }
-            return ResponseEntity.notFound().build();
+        checkCategoryExistenceById(id);
+        passCategoryRepository.deleteById(id);
+
+        return Response.successful("Deleted");
     }
     public ResponseEntity<String> updatePassCategory(PassCategoryResponseDTO passCategoryResponseDTO){
-            if (isCategoryExistById(passCategoryResponseDTO.id())) {
-                PassCategory updatedPass = passCategoryOf(passCategoryResponseDTO);
-                passCategoryRepository.saveAndFlush(updatedPass);
-                return ResponseEntity.ok("Pass category updated");
-            }
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pass category not found");
+        checkCategoryExistenceById(passCategoryResponseDTO.id());
+        PassCategory updatedPass = DtoMapper.toEntity(passCategoryResponseDTO);
+        passCategoryRepository.saveAndFlush(updatedPass);
+
+        return Response.successful("Updated");
     }
-    private PassCategory passCategoryOf(PassCategoryRegisterDTO passCategoryRegisterDTO){
-        return PassCategory.builder()
-                .passDuration(passCategoryRegisterDTO.passDuration())
-                .passExpireInDay(passCategoryRegisterDTO.passExpireInDay())
-                .category(passCategoryRegisterDTO.category())
-                .price(passCategoryRegisterDTO.price())
-                .build();
+    private void checkCategoryExistenceById(Long id){
+        passCategoryRepository.findById(id).orElseThrow(() -> new NotFoundException("Pass category with id " + id));
     }
-    private PassCategory passCategoryOf(PassCategoryResponseDTO passCategoryResponseDTO){
-        return PassCategory.builder()
-                .id(passCategoryResponseDTO.id())
-                .passDuration(passCategoryResponseDTO.passDuration())
-                .passExpireInDay(passCategoryResponseDTO.passExpireInDay())
-                .category(passCategoryResponseDTO.category())
-                .price(passCategoryResponseDTO.price())
-                .build();
-    }
-    private boolean isCategoryExistById(Long id){
-        return passCategoryRepository.findById(id).isPresent();
-    }
-    private boolean isCategoryNotExist(PassCategoryRegisterDTO passCategoryRegisterDTO){
-        return passCategoryRepository.findByCategoryAndPassDuration(passCategoryRegisterDTO.category(), passCategoryRegisterDTO.passDuration()).isEmpty();
+    private void validateCategoryNotExist(PassCategoryRegisterDTO passCategoryRegisterDTO) {
+        if (passCategoryRepository.findByCategoryAndPassDuration(passCategoryRegisterDTO.category(), passCategoryRegisterDTO.passDuration()).isPresent()) {
+            throw new DataIntegrityViolationException("Category already exist");
+        }
     }
 }
