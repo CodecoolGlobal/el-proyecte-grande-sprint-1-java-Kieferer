2d2a9a748ef83c7c04e9edbfff8d808d8c83f8e4
Norbert Remenyi <remenyinorbert@gmail.com>
2023-05-16 12:44:31 +0200

    Chore: PurchasedPassService use DtoMapper, code repetition replaced by method

--- src/main/java/com/codecool/budapestgo/service/PurchasedPassService.java
+++ src/main/java/com/codecool/budapestgo/service/PurchasedPassService.java
@@ -2,22 +2,23 @@ package com.codecool.budapestgo.service;
 
 import com.codecool.budapestgo.controller.dto.pass.PassDTO;
 import com.codecool.budapestgo.controller.dto.pass.PurchasedPassResponseDTO;
+import com.codecool.budapestgo.customExceptionHandler.NotFoundException;
 import com.codecool.budapestgo.dao.model.PassCategory;
 import com.codecool.budapestgo.dao.model.Client;
 import com.codecool.budapestgo.dao.repository.ClientRepository;
 import com.codecool.budapestgo.dao.model.PurchasedPass;
 import com.codecool.budapestgo.dao.repository.PassCategoryRepository;
 import com.codecool.budapestgo.dao.repository.PurchasedPassRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
+import com.codecool.budapestgo.utils.DtoMapper;
+import com.codecool.budapestgo.utils.Response;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class PurchasedPassService {
     private final PurchasedPassRepository purchasedPassRepository;
     private final PassCategoryRepository passCategoryRepository;
@@ -28,25 +29,6 @@ public class PurchasedPassService {
                 .stream()
                 .map(PurchasedPassResponseDTO::of)
                 .toList();
-    }
-
-    public ResponseEntity<String> addPass(PassDTO passDTO){
-        Optional<Client> client = clientRepository.findById(passDTO.clientId());
-        Optional<PassCategory> pass = passCategoryRepository.findByCategoryAndPassDuration(passDTO.passCategory(),passDTO.passDuration());
-
-        if(client.isPresent() && pass.isPresent()) {
-            long expireInDays = pass.get().getPassExpireInDay();
-            PurchasedPass purchasedPass = PurchasedPass.builder()
-                    .client(client.get())
-                    .passCategory(pass.get())
-                    .expireTime(PurchasedPass.calculateExpireTime(expireInDays))
-                    .build();
-
-            purchasedPassRepository.save(purchasedPass);
-            return ResponseEntity.ok("PassCategory have been purchased");
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body("Client or passCategory not found.");
     }
     public List<PurchasedPassResponseDTO> getExpiredPasses(Long id){
         return purchasedPassRepository.getAllExpiredPassesByClient_id(id)
@@ -60,10 +42,24 @@ public class PurchasedPassService {
                 .map(PurchasedPassResponseDTO::of)
                 .toList();
     }
-    public ResponseEntity<String> deletePassesByClientId(Long id){
-        return purchasedPassRepository.deleteAllByClient_Id(id) ?
-                ResponseEntity.ok("Passes have been deleted.") :
-                ResponseEntity.badRequest().body("There is no pass to delete.");
-    }
+    public ResponseEntity<String> addPass(PassDTO passDTO){
+        Client client = getClientById(passDTO.clientId());
+        PassCategory pass = getPassCategoryByNameAndDuration(passDTO.passCategory(), passDTO.passDuration());
+        PurchasedPass purchasedPass = DtoMapper.toEntity(client, pass);
 
+        purchasedPassRepository.save(purchasedPass);
+        return Response.successful("Purchased");
+    }
+    public ResponseEntity<String> deletePassesByClientId(Long id){
+        getClientById(id);
+        purchasedPassRepository.deleteAllByClient_Id(id);
+        return Response.successful("Passes of client deleted");
+    }
+    private Client getClientById(long id){
+        return clientRepository.findById(id).orElseThrow(() -> new NotFoundException("Client with id " + id));
+    }
+    private PassCategory getPassCategoryByNameAndDuration(String passCategory, String passDuration){
+        return  passCategoryRepository.findByCategoryAndPassDuration(passCategory, passDuration)
+                .orElseThrow(() -> new NotFoundException("Pass category "));
+    }
 }
