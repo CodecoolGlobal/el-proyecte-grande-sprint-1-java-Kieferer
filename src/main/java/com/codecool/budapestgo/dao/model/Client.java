af1ec0f72ebdd633cf67ddcf6b29a6d7e401a86a
Norbert Remenyi <remenyinorbert@gmail.com>
2023-05-22 19:12:44 +0200

    chore: add cascade to purchase pass

--- src/main/java/com/codecool/budapestgo/dao/model/Client.java
+++ src/main/java/com/codecool/budapestgo/dao/model/Client.java
@@ -1,12 +1,15 @@
 package com.codecool.budapestgo.dao.model;
 
 import com.codecool.budapestgo.dao.types.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Builder
@@ -25,6 +28,17 @@ public class Client implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PurchasedPass> purchasedPass = new ArrayList<>();
+    public void addPurchasedPass(PurchasedPass pass) {
+        purchasedPass.add(pass);
+        pass.setClient(this);
+    }
+    public void removePurchasedPass(PurchasedPass pass) {
+        purchasedPass.remove(pass);
+        pass.setClient(null);
+    }
     public void setPassword(@NonNull String password) {
         this.password = password;
     }
--- src/main/java/com/codecool/budapestgo/dao/repository/PurchasedPassRepository.java
+++ src/main/java/com/codecool/budapestgo/dao/repository/PurchasedPassRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PurchasedPassRepository extends JpaRepository<PurchasedPass,Long> {
-    @Query("SELECT p FROM PurchasedPass p WHERE p.expireTime < NOW() AND  p.client.id = :id")
-    List<PurchasedPass> getAllExpiredPassesByClient_id(Long id);
-    @Query("SELECT p FROM PurchasedPass p WHERE p.expireTime > NOW() AND  p.client.id = :id")
-    List<PurchasedPass> getActivePassesByClient_id(Long id);
-    int deleteAllByClient_Id(Long id);
+    @Query("SELECT p FROM PurchasedPass p WHERE p.expireTime < NOW() AND  p.client.email = :email")
+    List<PurchasedPass> getAllExpiredPassesByClient(String email);
+    @Query("SELECT p FROM PurchasedPass p WHERE p.expireTime > NOW() AND  p.client.email = :email")
+    List<PurchasedPass> getActivePassesByClient(String email);
+    @Query("SELECT p FROM PurchasedPass p WHERE p.client.email = :email")
+    List<PurchasedPass> findAllByByClient(String email);
+    int deleteAllByClient_Email(String email);
 }
