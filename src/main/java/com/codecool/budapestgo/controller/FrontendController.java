4c1d06ad77607b53df1464f8de56a968c582c46b
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-05-23 12:21:17 +0200

    fix: Remove "index.html" from request to avoid circulation

--- src/main/java/com/codecool/budapestgo/controller/FrontendController.java
+++ src/main/java/com/codecool/budapestgo/controller/FrontendController.java
@@ -2,11 +2,12 @@ package com.codecool.budapestgo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class FrontendController {
-    @GetMapping({"/", "/index.html"})
+    @GetMapping({"/index"})
     public String index() {
-        return "index.html";
+        return "index";
     }
 }
