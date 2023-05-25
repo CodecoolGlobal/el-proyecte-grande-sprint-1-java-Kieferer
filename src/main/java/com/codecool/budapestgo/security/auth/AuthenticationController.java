bcb3f42f448ed6ab1dcfc032a3f3d9cb6b52af73
klavoraMate <klavoramate98@gmail.com>
2023-05-25 10:21:20 +0200

    feat: created endpoint to handle frontend login request and empty redirect endpoint created

--- src/main/java/com/codecool/budapestgo/security/auth/AuthenticationController.java
+++ src/main/java/com/codecool/budapestgo/security/auth/AuthenticationController.java
@@ -3,16 +3,16 @@ package com.codecool.budapestgo.security.auth;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService service;
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
@@ -25,6 +25,32 @@ public class AuthenticationController {
             @RequestBody AuthenticationRequest request
     ) {
         return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @GetMapping("/oauth2/authorizationPageUrl/google")
+    public String getAuthorizationPageUrl(){
+        ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("google");
+        if (clientRegistration != null) {
+            String authorizationUri = clientRegistration.getProviderDetails().getAuthorizationUri();
+            String clientId = clientRegistration.getClientId();
+            String redirectUri ="http://localhost:8080/login/oauth2/code/";
+            String scope = String.join(" ", clientRegistration.getScopes());
+            String state = "xyz123"; // Generate and store a unique state value for CSRF protection
+
+            String authorizationUrl = String.format("%s?client_id=%s&redirect_uri=%s&scope=%s&response_type=code&state=%s",
+                    authorizationUri, clientId, redirectUri, scope, state);
+
+            return authorizationUrl;
+        }
+
+        // Handle error: Unable to find Google client registration
+
+        return null;
+    }
+
+    @GetMapping("/login/oauth2/code/{registrationId}")
+    public void handleOauth2Redirect(@PathVariable String registrationId){
+        System.out.println(registrationId);
     }
 
 }
