dcbf9461bf40ea2c14a63f737db9cfb599d7efeb
klavoraMate <klavoramate98@gmail.com>
2023-06-07 14:35:06 +0200

    feat: created test case for adding schedule

---
+++ src/test/java/com/codecool/budapestgo/service/ScheduleServiceTest.java
@@ -0,0 +1,83 @@
+package com.codecool.budapestgo.service;
+
+import com.codecool.budapestgo.controller.dto.schedule.ScheduleDTO;
+import com.codecool.budapestgo.dao.model.Route;
+import com.codecool.budapestgo.dao.model.Schedule;
+import com.codecool.budapestgo.dao.model.Stop;
+import com.codecool.budapestgo.dao.repository.ScheduleRepository;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ScheduleServiceTest {
+    @Mock
+    private ScheduleRepository scheduleRepository;
+
+    @Mock
+    private RouteService routeService;
+
+    @Mock
+    private StopService stopService;
+
+    private ScheduleService scheduleService;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+        scheduleService = new ScheduleService(scheduleRepository, routeService, stopService);
+    }
+
+    @Test
+    void testAddSchedule() {
+        Long routeId = 1L;
+        Long stopId = 1L;
+        ScheduleDTO scheduleDTO = new ScheduleDTO(routeId, stopId);
+
+        Route route = buildRoute(routeId, "Route 1");
+        Stop stop = buildStop(stopId, "Stop 1");
+
+        when(routeService.getRouteById(routeId)).thenReturn(route);
+        when(stopService.getStopById(stopId)).thenReturn(stop);
+
+        ResponseEntity<String> response = scheduleService.addSchedule(scheduleDTO);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Created successfully", response.getBody());
+        verify(scheduleRepository, times(1)).save(any(Schedule.class));
+        verify(stop, times(1)).addSchedule(any(Schedule.class));
+        verify(route, times(1)).addSchedule(any(Schedule.class));
+    }
+
+
+    private Route buildRoute(Long id, String name) {
+        return Route.builder()
+                .id(id)
+                .name(name)
+                .build();
+    }
+
+    private Stop buildStop(Long id, String name) {
+        return Stop.builder()
+                .id(id)
+                .name(name)
+                .build();
+    }
+
+    private Schedule buildSchedule(Long id, Route route, Stop stop) {
+        return Schedule.builder()
+                .id(id)
+                .route(route)
+                .stop(stop)
+                .build();
+    }
+
+    private void assertScheduleEquals(Schedule schedule, ScheduleDTO scheduleDTO) {
+        assertEquals(schedule.getRoute().getId(), scheduleDTO.routeId());
+        assertEquals(schedule.getStop().getId(), scheduleDTO.stopId());
+    }
+}
