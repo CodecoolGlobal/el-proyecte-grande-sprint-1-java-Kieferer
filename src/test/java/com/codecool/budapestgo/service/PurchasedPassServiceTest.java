9c6b27bc145056be7fbc46de4e11d93999063e26
klavoraMate <klavoramate98@gmail.com>
2023-06-08 11:34:59 +0200

    feat: created test case for adding PurchasedPass

--- src/test/java/com/codecool/budapestgo/service/PurchasedPassServiceTest.java
+++ src/test/java/com/codecool/budapestgo/service/PurchasedPassServiceTest.java
@@ -1,19 +1,27 @@
 package com.codecool.budapestgo.service;
 
+import com.codecool.budapestgo.controller.dto.pass.PassDTO;
 import com.codecool.budapestgo.controller.dto.pass.PurchasedPassResponseDTO;
+import com.codecool.budapestgo.dao.model.Client;
 import com.codecool.budapestgo.dao.model.PassCategory;
 import com.codecool.budapestgo.dao.model.PurchasedPass;
 import com.codecool.budapestgo.dao.repository.PurchasedPassRepository;
+import com.codecool.budapestgo.utils.DtoMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class PurchasedPassServiceTest {
@@ -82,7 +90,26 @@ class PurchasedPassServiceTest {
             assertPurchasedPassEquals(activePasses.get(i), result.get(i));
         }
     }
+    @Test
+    void testAddPass() {
+        String email = "test@example.com";
+        String passCategory = "Category 1";
+        String passDuration = "Duration 1";
+        PassDTO passDTO = new PassDTO(email, passCategory, passDuration);
+        Client client = buildClient(email);
+        PassCategory passCategoryObj = buildPassCategory(passCategory);
+        PurchasedPass purchasedPass = DtoMapper.toEntity(client, passCategoryObj);
 
+        when(clientService.getClientByEmail(email)).thenReturn(client);
+        when(passCategoryService.getPassCategoryByNameAndDuration(anyString(), anyString())).thenReturn(passCategoryObj);
+        when(purchasedPassRepository.save(purchasedPass)).thenReturn(purchasedPass);
+
+        ResponseEntity<String> response = purchasedPassService.addPass(passDTO);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Purchased successfully", response.getBody());
+        verify(purchasedPassRepository).save(any(PurchasedPass.class));
+    }
     private PurchasedPass buildPurchasedPass(String category, LocalDate expirationDate) {
         PassCategory passCategory = buildPassCategory(category);
         return PurchasedPass.builder()
@@ -94,9 +121,16 @@ class PurchasedPassServiceTest {
     private PassCategory buildPassCategory(String category) {
         return PassCategory.builder()
                 .category(category)
-                .passDuration("Duration")
+                .passDuration("Duration 1")
                 .passExpireInDay(10L)
                 .price(10)
+                .build();
+    }
+    private Client buildClient(String email) {
+        return Client.builder()
+                .email(email)
+                .password("password")
+                .purchasedPass(new ArrayList<>())
                 .build();
     }
 
